b5c04b651c5317f09a02d05e97bda3a6955bd001
kelvinrevertis <kelvinrev2@hotmail.com>
2022-07-03 15:17:36 -0300

    Commit 3.0

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+
 let openForm =()=>{
     console.log("EEEE")
     let form = document.getElementById('cad-form')
     form.style.display = "block"
+}
+
+let testClient ={
+    nome: "Revertis",
+    email: "kelvinrev2@hotmail.com",
+    date:"06/10/1995",
+    tel:"33987046181"
+}
+
+let getLocalStorage = ()=> JSON.parse(localStorage.getItem('data')) ?? []
+let setLocalStorage = (data) => localStorage.setItem("data", JSON.stringify(data))
+
+let saveClient = (client) =>{
+    const data = getLocalStorage()
+    console.log(data)
+    data.push(client)
+
+}
+
+let editClient = (index,client)=>{
+    let edit = getLocalStorage()
+    edit[index] = client
+    setLocalStorage(edit)
+    
 }
